bfbc8fcdb7f54feb28213b89724e1927167fbfcd
Chris <c.nethercott@icloud.com>
2016-09-16 11:33:59 +0100

    Update ArduinoTimer.ino

--- src/ArduinoTimer/ArduinoTimer.ino
+++ src/ArduinoTimer/ArduinoTimer.ino
@@ -3,8 +3,17 @@
   ArduinoTimer
 */
 
-void setup() {
+long startTime;
+long endTime;
+long elapsedTime;
 
+void setup() {
+  startTime = millis();
+  delay(1000);
+  endTime = millis();
+  
+  elapsedTime = startTime-endTime;
+  Serial.println(elapsedTime);
 }
 
 void loop() {
